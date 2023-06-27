a5238ed12ff3422628417103c64a0b0f9c5adf2a
Cristian Andres <cristianvcx188@gmail.com>
2023-06-26 21:48:20 -0400

    digitic

---
+++ models/productModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose"); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  slug: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Category",
+  },
+  quantity: Number,
+  images: {
+    type: Array,
+  },
+  color: {
+    type: String,
+    enum: ["Black", "Brown", "Red"],
+  },
+});
+
+//Export the model
+module.exports = mongoose.model("Product", userSchema);
