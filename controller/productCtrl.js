3132ee5ebecd46c64bd8b8ed03e2cef92785b791
Cristian Andres <cristianvcx188@gmail.com>
2023-06-27 23:36:28 -0400

    digitic

---
+++ controller/productCtrl.js
@@ -0,0 +1,37 @@
+const Product = require("../models/productModel");
+const asyncHandler = require("express-async-handler");
+const slugify = require("slugify");
+
+const createProduct = asyncHandler(async (req, res) => {
+  try {
+    if (req.body.title) {
+      req.body.slug = slugify(req.body.title);
+    }
+    const newProduct = await Product.create(req.body);
+    res.json(newProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+//2:50:20
+
+const getaProduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  try {
+    const findProduct = await Product.findById(id);
+    res.json(findProduct);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getAllProduct = asyncHandler(async (req, res) => {
+  try {
+    const getallProducts = await Product.find();
+    res.json(getallProducts);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+module.exports = { createProduct, getaProduct, getAllProduct };
--- models/productModel.js
+++ models/productModel.js
@@ -1,39 +1,60 @@
 const mongoose = require("mongoose"); // Erase if already required
 
 // Declare the Schema of the Mongo model
-var productSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
+var productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    sold: {
+      type: Number,
+      default: 0,
+    },
+
+    images: {
+      type: Array,
+    },
+    color: {
+      type: String,
+      required: true,
+    },
+    ratings: [
+      {
+        star: Number,
+        postedby: { type: mongoose.Schema.Types.ObjectId, red: "User" },
+      },
+    ],
   },
-  slug: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  category: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Category",
-  },
-  quantity: Number,
-  images: {
-    type: Array,
-  },
-  color: {
-    type: String,
-    enum: ["Black", "Brown", "Red"],
-  },
-});
+  { timestamps: true }
+);
 
 //Export the model
-module.exports = mongoose.model("Product", userSchema);
+module.exports = mongoose.model("Product", productSchema);
