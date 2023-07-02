8c1ab4cdb63b07fe6cbb7353ccdb1fea6a8b598f
Cristian Andres <cristianvcx188@gmail.com>
2023-07-02 02:36:06 -0400

    digitic

--- controller/userCtrl.js
+++ controller/userCtrl.js
@@ -189,7 +189,19 @@ const unblockUser = asyncHandler(async (req, res) => {
     throw new Error(error);
   }
 });
-
+const updatePassword = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const { password } = req.body;
+  validateMongoDbId(_id);
+  const user = await User.findById(_id);
+  if (password) {
+    user.password = password;
+    const updatedPassword = await user.save();
+    res.json(updatedPassword);
+  } else {
+    res.json(user);
+  }
+});
 module.exports = {
   createUser,
   loginUserCtrl,
@@ -201,4 +213,5 @@ module.exports = {
   unblockUser,
   handleRefreshToken,
   logout,
+  updatePassword,
 };
