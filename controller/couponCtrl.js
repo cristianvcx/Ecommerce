b2adfa65c99ff74a489eed6b37a012323ed0060e
Cristian Andres <cristianvcx188@gmail.com>
2023-07-15 03:09:50 -0400

    digitic

---
+++ controller/couponCtrl.js
@@ -0,0 +1,23 @@
+const Coupon = require("../models/couponModel");
+const validateMongoDbId = require("../utils/validateMongodbId");
+const asynHandler = require("express-async-handler");
+
+const createCoupon = asynHandler(async (req, res) => {
+  try {
+    const newCoupon = await Coupon.create(req.body);
+    res.json(newCoupon);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const getAllCoupons = asynHandler(async (req, res) => {
+  try {
+    const coupons = await Coupon.find();
+    res.json(coupons);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = { createCoupon, getAllCoupons };
